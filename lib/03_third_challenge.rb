54bf5273dce30ea93be72eb6fd167a3437671ee6
Fnu Tsering Norbu <thetseringnorbu@gmail.com>
2020-04-24 04:33:21 +0000

    Automatically backed up by Learn

--- lib/03_third_challenge.rb
+++ lib/03_third_challenge.rb
@@ -30,9 +30,12 @@ def third_challenge
         #your key/value pairs here
         },
       :heroine => {
+        :name => "Juliet",
+        :age => "15",
+        :status => "alive"
         #your key/value pairs here
         },
-      :heroine_friends => []
+      :heroine_friends => [{}, {}]
    }
   }
 
